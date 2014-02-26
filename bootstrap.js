f83bc43ee5a092930ff27e62d4266a7f56a62b3c
Spencer Schack <scs1818@gmail.com>
2014-02-26 13:32:29 -0800

    added jquery loader

--- bootstrap.js
+++ bootstrap.js
@@ -1,7 +1,16 @@
-(function(Fyoosha) {
+window.Fyoosha = window.Fyoosha || (function() {
 
-	if(!window.jQuery) {
-		console.log("no jquery");
+	function loadScript(src) {
+		var script = document.createElement("script");
+		script.type = "text/javascript";
+		script.src = src;
+		document.head.appendChild(script);
 	}
 
-})({});
+	if(!window.jQuery) {
+		loadScript("//code.jquery.com/jquery-2.1.0.min.js");
+	}
+
+	return this;
+
+}).call({});
